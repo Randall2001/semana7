e2987244ac9b7c9715e6f7dfe2d8aefc42a1f123
Randall2001 <134986191+Randall2001@users.noreply.github.com>
2023-07-02 18:26:56 -0600

    1

---
+++ ManejoMenu/src/App.java
@@ -0,0 +1,9 @@
+import BussinessLogic.Menu;
+//**La calse .app es la que inicia nuestra aplicacion */
+public class App {
+ 
+    public static void main(String[] args) throws Exception {
+        //new Menu();
+        new Menu();
+    }
+}
